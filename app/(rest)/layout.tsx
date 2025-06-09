a6cffe339d6164f489d89ef8f6cb1b61b46202c2
Nadeem-2005 <mr.maestro002@gmail.com>
2025-06-09 12:22:33 +0530

    minor changes

--- app/(rest)/layout.tsx
+++ app/(rest)/layout.tsx
@@ -1,4 +1,20 @@
 import React from "react";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CliniCall-Dashboard",
+  description: "Your,  Health, Our Priority",
+};
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
