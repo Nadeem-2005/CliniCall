4541a93025d9aed60739a0da0190b8db3002fa87
Nadeem-2005 <mr.maestro002@gmail.com>
2025-12-30 14:12:43 +0530

    minor changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -36,20 +36,14 @@
 // });
 
 // export default prisma;
-// lib/prisma.ts
 import { PrismaClient } from "../app/generated/prisma";
 
 declare global {
-  // Prevent multiple instances in development
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-const prisma =
-  globalThis.prisma ??
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
+const prisma = globalThis.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
